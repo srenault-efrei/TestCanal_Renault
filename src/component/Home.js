386338dd28f4d3d8060e1a57ca1fb05911c8cc3d
Steven Renault <steven@MacBook-Air-de-Steven.local>
2020-05-08 20:27:15 +0200

    fixe project wihout hooks

--- src/component/Home.js
+++ src/component/Home.js
@@ -10,7 +10,9 @@ export default class Home extends React.Component {
         this.state = {
             films: [],
             genres: [],
-            apiKey: '92b418e837b833be308bbfb1fb2aca1e'
+            apiKey: '92b418e837b833be308bbfb1fb2aca1e',
+            targetValue: '',
+            allFilms: []
         }
     }
 
@@ -18,17 +20,17 @@ export default class Home extends React.Component {
     componentDidMount() {
         this.fetchFilsm()
         this.fetchGenres()
+        document.body.style = "background: black;";
     }
 
     fetchFilsm = async () => {
         return fetch(`https://api.themoviedb.org/3/discover/tv?api_key=${this.state.apiKey}&language=en-US&sort_by=popularity.desc&page=1&timezone=America/New_York&include_null_first_air_dates=false`)
             .then((response) => response.json())
             .then((json) => {
-
                 this.setState({
-                    films: json.results
+                    films: json.results,
+                    allFilms: json.results
                 })
-
             })
             .catch((error) => {
                 console.error(error);
@@ -39,7 +41,6 @@ export default class Home extends React.Component {
         return fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${this.state.apiKey}&language=fr`)
             .then((response) => response.json())
             .then((json) => {
-
                 this.setState({ genres: json.genres }, this.getGenre())
             })
             .catch((error) => {
@@ -50,10 +51,8 @@ export default class Home extends React.Component {
 
 
 
-    getGenre(id = null) {
-
+    getGenre = (id = null) => {
         let { genres } = this.state
-
         if (id) {
             for (const genre of genres) {
                 if (genre.id === id) {
@@ -63,21 +62,58 @@ export default class Home extends React.Component {
         }
     }
 
+    handleChange = (event) => {
+
+        let { allFilms } = this.state
+        let filmByName = []
+        let targetValue = event.target.value
+
+        for (const film of allFilms) {
+            if (film.original_name.match(targetValue)) {
+                filmByName.push(film)
+                this.setState({
+                    films: filmByName
+                })
+            }
+        }
+    }
+
+
+    selectChange = (event) => {
+        let targetValue = event.target.value
+        let filterFilms = this.state.allFilms
+
+        if( targetValue === 'first_air_date'){
+        filterFilms.sort((a,b) => `${b.first_air_date.slice(0,4)}${b.first_air_date.slice(5,7)}` - `${a.first_air_date.slice(0,4)}${a.first_air_date.slice(5,7)}`)
+        }else if (targetValue === 'popularity'){
+            filterFilms.sort((a,b) => b.popularity - a.popularity)
+
+        }
+        this.setState({
+            films: filterFilms
+        })
+    }
+
     render() {
+
         const films = this.state.films
+      
         return (
 
             <div className='content'>
+
+            
                 <h3 className='title'>Les films les plus pouplaires</h3>
-                <input className="form-control input" type="text" placeholder="Search" aria-label="Search"></input>
+                <div className='search-div'>
+                    <input className=" input" type="text" placeholder="Rechercher des flims" aria-label="Search" onChange={this.handleChange}></input>
+                </div>
 
                 <label className='label-select' for="filter-select">Trier par :</label>
 
-                <select name="filter" id="filter-select">
+                <select name="filter" id="filter-select" onChange = {this.selectChange}>
                     <option value="">-- Choisis une option --</option>
                     <option value="popularity">Popularité</option>
-                    <option value="date">Date</option>
-                    <option value="genre">Genre</option>
+                    <option value="first_air_date">Date</option>
                 </select>
 
 
@@ -86,9 +122,9 @@ export default class Home extends React.Component {
                         films.map((film, indexFilm) => (
 
                             <div key={indexFilm} className="card">
-                                <img className="card-img-top" src={`https://image.tmdb.org/t/p/original${film.poster_path}`} alt="Card image film"></img>
+                                <img className="card-img-top" src={`https://image.tmdb.org/t/p/original${film.poster_path}`} alt="Card film"></img>
                                 <div className="card-body">
-                                    <h5 className="card-title">{film.original_name}</h5>
+                                    <h5 className="card-title">{film.name}</h5>
 
                                     <div className="div-genres">
                                         {
@@ -97,7 +133,7 @@ export default class Home extends React.Component {
                                             ))
                                         }
                                     </div>
-
+                                   <span className ='popularity'> {film.popularity.toFixed()}</span>
                                     <p className="card-text">
                                         <Moment format="DD/MM/YYYY">
                                             {film.first_air_date}
@@ -108,6 +144,7 @@ export default class Home extends React.Component {
                         ))
                     }
                 </div>
+
             </div>
         );
     }
